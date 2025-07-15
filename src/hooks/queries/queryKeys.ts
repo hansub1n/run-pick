23a62a79d125e9eb854f4f9a41d58611026b80e2
hansub1n <ella0411@naver.com>
2025-07-15 15:19:41 +0900

    fix: ESLint 경고 및 에러 수정

--- src/hooks/queries/queryKeys.ts
+++ src/hooks/queries/queryKeys.ts
@@ -6,4 +6,5 @@ export const QUERY_KEYS = {
   user: (userId: string) => ['user', userId],
   videos: (distance: Distance) => ['videos', distance],
   challenges: (level: Level) => ['challenges', level],
+  // videoDetail: (videoId: ParamValue) => ['videoDetail', videoId],
 };
