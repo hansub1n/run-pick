2f50d45d5ade83894c28ffafce374a76530af5b2
hansub1n <ella0411@naver.com>
2025-11-19 20:42:02 +0900

    chore: 불필요한 파일 삭제

--- src/app/run-proof/[id]/edit/page.tsx
+++
@@ -1,8 +0,0 @@
-import RunProofForm from '@/components/run-proof/RunProofForm';
-
-// TODO: 폼에 존재하는 데이터 삽입
-const RunProofEditPage = () => {
-  return <RunProofForm />;
-};
-
-export default RunProofEditPage;
