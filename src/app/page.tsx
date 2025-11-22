40f0ee063eff2c33e90d5f391f6e6ab084940afa
hansub1n <ella0411@naver.com>
2025-11-22 15:06:16 +0900

    fix: 탑러너 유저 정보 서버 -> zustand 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,14 @@ import ChallengePromptSection from '@/components/home/ChallengePromptSection';
 import MotivationSection from '@/components/home/MotivationSection';
 import TopRunnersSection from '@/components/home/TopRunnersSection';
 import TopVideosSection from '@/components/home/TopVideosSection';
-import { getPublicUserInfo } from '@/utils/supabase/server';
 
-export default async function Home() {
-  const userInfo = await getPublicUserInfo();
-
+export default function Home() {
   return (
     <div className='w-[313px] flex flex-col gap-[23px] py-[23px]'>
-      <MotivationSection userInfo={userInfo} />
+      <MotivationSection />
       <TopVideosSection />
       <ChallengePromptSection />
-      <TopRunnersSection userId={userInfo.id} />
+      <TopRunnersSection />
     </div>
   );
 }
